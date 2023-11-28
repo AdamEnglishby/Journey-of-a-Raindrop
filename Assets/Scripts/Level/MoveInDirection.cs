f0e418c975a9603d17b93fd959c4829141b54f8f
Adam Englishby <adam@englishby.ie>
2023-11-28 15:09:38 +0000

    feat(intro): opening cutscene redux

---
+++ Assets/Scripts/Level/MoveInDirection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Level
+{
+    public class MoveInDirection : MonoBehaviour
+    {
+        [SerializeField] private Vector3 direction;
+        
+        private Transform _transform;
+
+        private void Start()
+        {
+            this._transform = transform;
+        }
+
+        private void Update()
+        {
+            _transform.position += direction * Time.deltaTime;
+        }
+    }
+}
